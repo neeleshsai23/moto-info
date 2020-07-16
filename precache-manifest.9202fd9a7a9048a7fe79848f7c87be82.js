4c0836887556066c11d02c0fd98ed77ba4e1725c
Neelesh Sai <rneeleshsai@gmail.com>
2020-07-17 00:35:46 +0530

    Updates

---
+++ precache-manifest.9202fd9a7a9048a7fe79848f7c87be82.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d3263723dce950dbde746a3aa9ce534",
+    "url": "/moto-info/index.html"
+  },
+  {
+    "revision": "67a3cedba0b0bcd78a7d",
+    "url": "/moto-info/static/css/main.598f49b4.chunk.css"
+  },
+  {
+    "revision": "f992c1fdcc9ee2650699",
+    "url": "/moto-info/static/js/2.599c6a75.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/moto-info/static/js/2.599c6a75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67a3cedba0b0bcd78a7d",
+    "url": "/moto-info/static/js/main.bf22a8d8.chunk.js"
+  },
+  {
+    "revision": "51a6654f50971b432412",
+    "url": "/moto-info/static/js/runtime-main.1895315a.js"
+  },
+  {
+    "revision": "539603f0f7100d1c12b8554a57e8f24a",
+    "url": "/moto-info/static/media/CompressionRatio.539603f0.png"
+  },
+  {
+    "revision": "c2dc018b66faabf748b4b8c2511959fc",
+    "url": "/moto-info/static/media/StrokeCycle.c2dc018b.jpg"
+  },
+  {
+    "revision": "bbbf09172ea07322c63aecb27ad7aa3a",
+    "url": "/moto-info/static/media/boxer.bbbf0917.jpg"
+  },
+  {
+    "revision": "344a6ab31cb85a1aa234b76e0c0370c2",
+    "url": "/moto-info/static/media/infour.344a6ab3.gif"
+  },
+  {
+    "revision": "66d0056e7e730b32f1b6db3addcd2cf7",
+    "url": "/moto-info/static/media/ivsp.66d0056e.webp"
+  },
+  {
+    "revision": "b844f884ffc5843a710f2c4322cc3713",
+    "url": "/moto-info/static/media/single.b844f884.jpg"
+  },
+  {
+    "revision": "a845a1b2109c077aa8d7dc920e995e00",
+    "url": "/moto-info/static/media/vtwin.a845a1b2.jpg"
+  }
+]);
