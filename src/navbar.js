fa1af2ee3ede0e58aff6f3871877fe05091a500a
Neelesh Sai <rneeleshsai@gmail.com>
2020-09-02 17:07:38 +0530

    routing changed

--- src/navbar.js
+++ src/navbar.js
@@ -24,7 +24,7 @@ class Navbar extends Component {
       <div>
         <nav className='nav-bar'>
           <div className='navitem'>
-            <NavLink exact to='/' className='navlink'>
+            <NavLink exact to='' className='navlink'>
               Home
             </NavLink>
           </div>
